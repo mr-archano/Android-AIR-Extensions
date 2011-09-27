2095506f6981d1737f53e5fb1a1bc23ed0685d99
anto <archano@gmail.com>
2011-09-27 22:34:26 +0200

    ADDED: res/ folder (mandatory even if emtpy for Android build process) ADDED: libs/ folder (can't directly give out AIR runtime extensions jar) ADDED: README files FIXED: .classpath content (now includes libs/FlashRuntimeExtensions.jar)

---
+++ AndroidExtensions_Native/libs/README.txt
@@ -0,0 +1,2 @@
+Copy here the FlashRuntimeExtensions.jar provided in the AIR 3.0 sdk.
+By default it is located in $AIR_SDK_HOME/lib/android/
---
+++ AndroidExtensions_Native/src/com/ssd/android/ane/AndroidExtensionsContext.java
@@ -0,0 +1,23 @@
+package com.ssd.android.ane;
+
+import java.util.Map;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+
+public class AndroidExtensionsContext extends FREContext {
+	public static final String TAG = AndroidExtensions.TAG;
+	
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Map<String, FREFunction> getFunctions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
