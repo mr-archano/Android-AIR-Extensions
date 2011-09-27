fc197bbf1b7b03b60b77d53935074bd250624e57
anto <archano@gmail.com>
2011-09-27 22:34:26 +0200

    ADDED: res/ folder (mandatory even if emtpy for Android build process) ADDED: libs/ folder (can't directly give out AIR runtime extensions jar) ADDED: README files FIXED: .classpath content (now includes libs/FlashRuntimeExtensions.jar)

--- AndroidExtensions_Native/src/com/ssd/android/ane/AndroidExtensions.java
+++ AndroidExtensions_Native/src/com/ssd/android/ane/AndroidExtensions.java
@@ -1,5 +1,30 @@
 package com.ssd.android.ane;
 
-public class AndroidExtensions {
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREExtension;
+
+public class AndroidExtensions implements FREExtension {
+	public static final String TAG = "AndroidExtensions";
+	
+	@Override
+	public FREContext createContext(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 
 }
